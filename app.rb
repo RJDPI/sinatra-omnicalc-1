a44bf5c3adcdc57c6376a659433c0022693755a2
Ricardo Jimenez <124076987+RJDPI@users.noreply.github.com>
2023-09-24 22:29:04 +0000

    stuck on math

--- app.rb
+++ app.rb
@@ -1,12 +1,39 @@
 require "sinatra"
 require "sinatra/reloader"
 
-get("/howdy") do
-  erb(:hello)
+get("/") do
+  "
+  <h1>Welcome to your Sinatra App!</h1>
+  <p>Define some routes in app.rb</p>
+  "
 end
 
-get("/goodbye") do
-  erb(:bye)
+get("/square_root/new") do
+  erb(:new_square_root_calc)
+end
+
+get("/square_root/results") do
+  @the_num = params.fetch("users_number").to_f
+
+  @the_result = Math.sqrt(@the_num).to_f
+  erb (:square_root_results)
+end
+
+get("/payment/new") do
+  erb(:payment_calc)
+end
+
+get("/payment/results") do
+  
+  @principal = params.fetch("users_principal").to_f
+  @apr = params.fetch("users_apr").to_f / 100
+  @years = params.fetch("users_year").to_f * 12
+
+  @top = @apr * (@principal)
+  @bottom = 1 - ((1 + @apr) ** (-1 * @years))
+
+  @payment = @top / @bottom
+  erb(:payment_calc_results)
 end
 
 
@@ -14,13 +41,22 @@ get("/square/new") do
   erb(:new_square_calc)
 end
 
-get '/square/results' do
+get("/square/results") do
+  @the_num=params.fetch("users_number").to_f
+
+  @the_result=@the_num ** 2
   erb(:square_results)
 end
 
-get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+get("/random/new") do
+  erb(:random)
+end
+
+get("/random/results") do
+@min = params.fetch("user_min").to_f
+@max = params.fetch("users_max").to_f
+
+@random_number = rand(@min..@max)
+erb(:random_results)
+
 end
